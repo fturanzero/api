fdbcaee8c3742f15475b75103009b0f9bed46dfa
fturanzero <60838036+fturanzero@users.noreply.github.com>
2021-01-04 00:12:08 +0300

    Update thycmd.sh

--- thycotic/thycmd.sh
+++ thycotic/thycmd.sh
@@ -12,5 +12,6 @@ launcher_req="{\"launcherid\":${launcherid},\"promptFieldValue\":\"${machine}\",
 
 token=$(curl -s -k -X POST -H 'Content-Type: application/x-www-form-urlencoded' -d $token_req $site'/oauth2/token' | jq -r '.access_token')
 ssUrl=$(curl -s -k -X POST -H 'Authorization: Bearer '$token -H $'Content-Type: application/json' -d $launcher_req $site'/api/v1/launchers/secret' | jq -r '.model.ssUrl')
+$ssLink="sslauncher:///${ssUrl}"
 
-echo 'sslauncher:///'$ssUrl
+echo ssLink
