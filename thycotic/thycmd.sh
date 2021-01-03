b1ff456700b08fc712b8dfa4e08d12ee82909f07
fturanzero <60838036+fturanzero@users.noreply.github.com>
2021-01-04 00:09:34 +0300

    Create thycmd.sh

---
+++ thycotic/thycmd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+site='<url>'
+pass='<pass>'
+user=$1
+secretid=$2
+launcherid=$3
+machine=$4
+
+token_req="username=${user}&grant_type=password&password=${pass}"
+launcher_req="{\"launcherid\":${launcherid},\"promptFieldValue\":\"${machine}\",\"secretid\":${secretid}}"
+
+token=$(curl -s -k -X POST -H 'Content-Type: application/x-www-form-urlencoded' -d $token_req $site'/oauth2/token' | jq -r '.access_token')
+ssUrl=$(curl -s -k -X POST -H 'Authorization: Bearer '$token -H $'Content-Type: application/json' -d $launcher_req $site'/api/v1/launchers/secret' | jq -r '.model.ssUrl')
+
+echo 'sslauncher:///'$ssUrl
